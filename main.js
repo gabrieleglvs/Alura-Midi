92aeff1d864785d843465928ff264bfbf30b8437
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-01-24 22:07:16 -0300

    trocando while por for

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ function tocaSom (idElementoAudio) {
 //CRIEI UMA REFERÊNCIA
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while(contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1]; //usando o classList eu acessei a lista de classe do elemento que selecionei uma o listaDeTeclas[contador]
@@ -15,13 +13,7 @@ while(contador < listaDeTeclas.length) {
     //TEPLATE STRING
     const idAudio = `#som_${instrumento}`;
 
-    //console.log(idAudio);
-
     tecla.onclick = function () {
         tocaSom(idAudio)
     }; //criei uma funcao anonima
-
-    contador++;
-
-    //console.log(contador);
 }
