55fd4336e15c25d928824a7db1fa14bb7ca1653d
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-01-25 19:56:31 -0300

    melhorando o código - corrigindo erros

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play(); //toca o som da tecla pom
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio); //toca o som da tecla pom
+
+    if(elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }  else {
+        alert('Elemento não encontrado ou seletor inválido');
+        console.log('Elemento não encontrado');
+    }
 }
 
 //CRIEI UMA REFERÊNCIA
